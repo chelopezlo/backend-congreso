e8285578412a5dd79afb0546ac40bce1b931f00e
Eliecer Lopez <eliecer.lopez.lopez@gmail.com>
2016-10-29 04:26:23 -0300

    Timeline Twitter

--- resources/views/countdown.php
+++ resources/views/countdown.php
@@ -61,6 +61,10 @@ h1{
     <div class="smalltext">Segundos</div>
   </div>
 </div>
+<br />
+            <a class="twitter-timeline"  href="https://twitter.com/hashtag/esencia2016" data-widget-id="792213148585582592">Tweets sobre #esencia2016</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+          
 
 <script src="//assets.codepen.io/assets/common/stopExecutionOnTimeout-02ca87916b03e6c52eed864acfc0a31a.js"></script>
 <script>function getTimeRemaining(endtime) {
