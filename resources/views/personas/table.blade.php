b5da41667429de0c14489638aed128d6274dcc2a
Eliecer Lopez <eliecer.lopez.lopez@gmail.com>
2016-10-29 04:33:55 -0300

    Correción tabla personas

--- resources/views/personas/table.blade.php
+++ resources/views/personas/table.blade.php
@@ -1,22 +1,11 @@
 <table class="table table-responsive" id="personas-table">
     <thead>
         <th>Rut</th>
-        <th>Code</th>        
-        <th>Full Name</th>
-        <th>Gender</th>
-        <th>Birthday</th>
-        <th>Occupation</th>
-        <th>Address</th>
-        <th>Phone</th>
-        <th>Email</th>
-        <th>Description</th>
-        <th>Facebook</th>
-        <th>Twitter</th>
-        <th>Users Id</th>
-        <th>Is Leader</th>
-        <th>Iglesias Id</th>
-        <th>Created At</th>
-        <th>Updated At</th>
+        <th>QR</th>        
+        <th>Nombre</th>
+        <th>Genero</th>
+        <th>Descripci&oacute;n</th>
+        <th>L&iacute;der</th>
         <th colspan="3">Action</th>
     </thead>
     <tbody>
@@ -26,27 +15,13 @@
             <td>{!! $persona->code !!}</td>
             <td>{!! $persona->full_name !!}</td>
             <td>{!! $persona->gender !!}</td>
-            <td>{!! $persona->birthday !!}</td>
-            <td>{!! $persona->occupation !!}</td>
-            <td>{!! $persona->address !!}</td>
-            <td>{!! $persona->phone !!}</td>
-            <td>{!! $persona->email !!}</td>
             <td>{!! $persona->description !!}</td>
-            <td>{!! $persona->facebook !!}</td>
-            <td>{!! $persona->twitter !!}</td>
-            <td>{!! $persona->users_id !!}</td>
             <td>{!! $persona->is_leader !!}</td>
-            <td>{!! $persona->iglesias_id !!}</td>
-            <td>{!! $persona->created_at !!}</td>
-            <td>{!! $persona->updated_at !!}</td>
             <td>
-                {!! Form::open(['route' => ['personas.destroy', $persona->id], 'method' => 'delete']) !!}
                 <div class='btn-group'>
                     <a href="{!! route('personas.show', [$persona->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
-                    <a href="{!! route('personas.edit', [$persona->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
-                    {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                    <a href="{!! route('personas.edit', [$persona->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>                   
                 </div>
-                {!! Form::close() !!}
             </td>
         </tr>
     @endforeach
